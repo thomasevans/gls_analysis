a3c77b6423419dcdefa6fd2e77fa09d668584897
Tom Evans <thomas.jude.evans@gmail.com>
2013-11-17 11:32:20 +0100

    Applying GeoLight to guillemot GLS data

---
+++ GeoLight_guillemot_example.R
@@ -0,0 +1,5 @@
+# Written by Tom Evans <thomas.jude.evans@gmail.com>, but based on code from
+# tamara.emmenegger@vogelwarte.ch from a workshop at Lund University in October 2013.
+
+# Here I use the GeoLight package to analyse geolocator data from a common guillemot using the GeoLight R package.
+
